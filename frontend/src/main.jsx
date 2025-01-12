5fb1eeafc95765f77327788498150aa21dd88c93
R.L.D.T.H. Surasinghe <surasinghe_rldth_e23@engug.ruh.ac.lk>
2025-01-12 18:37:12 +0530

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 import  'sweetalert2/dist/sweetalert2.js'
-
+import React from 'react'
+import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
