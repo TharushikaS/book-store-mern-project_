c82f2cb4a7517d49154bde9a9e5f32c686958ceb
Tharushika Surasinghe <146325963+TharushikaS@users.noreply.github.com>
2025-04-02 13:24:14 +0530

    Update main.tf

--- frontend/main.tf
+++ frontend/main.tf
@@ -2,7 +2,14 @@ provider "aws" {
   region = "us-east-1"  # Replace with your preferred region
 }
 
+# S3 Bucket
 resource "aws_s3_bucket" "bookstore_bucket" {
   bucket = "my-bookstore-bucket"
-  acl = "private"
+  acl    = "private"
+}
+
+# EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-071226ecf16aa7d96"  # Replace with a valid AMI ID
+  instance_type = "t2.micro"  # Choose the desired instance type
 }
